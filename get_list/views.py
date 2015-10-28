09641a6475a625fdd392c833dc5223fba80dc553
poojapauskar <git.poojapauskar@gmail.com>
2015-10-28 13:13:08 +0530

    get_list using access token

--- get_list/views.py
+++ get_list/views.py
@@ -9,29 +9,53 @@ from rest_framework import generics
 from django.shortcuts import get_object_or_404
 from django.db.models import Count 
 
-# class Get_listList(generics.ListCreateAPIView):
-#  queryset = Ticket.objects.all()
-#  serializer_class = Get_listSerializer
+
+
+
+
+
+class Get_listList(generics.ListCreateAPIView):
+ queryset = Ticket.objects.all()
+ serializer_class = Get_listSerializer
  # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
-def get_queryset(request):
-  access_token = request.GET.get('access_token')
-  import sys
-  print >> sys.stderr, access_token
+class Get_listDetail(generics.ListAPIView):
+ serializer_class = Get_listSerializer
 
-  #vz_id = self.kwargs['vz_id']
-     
-  vz_id= Register.objects.filter(token_generated=access_token).values_list('vz_id',flat=True)[0]
+ def get_queryset(self):
+  vz_id = self.kwargs['vz_id']
+        
+  #contacts= Friends.objects.filter(vz_id=vz_id).values('friends_vz_id')
+        
+        #vz_id= Register.objects.filter(phone=contacts).values('vz_id')
   #tickets = Ticket.objects.filter(vz_id__in=contacts)
-  print >> sys.stderr, vz_id
+
+  #detail=[]    
+  def ValuesQuerySetToDict(vqs):
+    return [item for item in vqs]
 
   import json
   friends_list=list(sync_contact.encode("utf8") for sync_contact in Sync.objects.filter(vz_id=vz_id).values_list('friends_vz_id',flat=True))
 
 
+  #friends_list = json.dumps(friends_list)
+
+  #friends_list = json.dumps(friends_list)
+  #print friends_list.query      
+  #friends_list = json.dumps(friends_list)
+       
+  #friends_list=list(friends_list)
+
+  import operator
+  from django.db.models import Q
+
   import datetime
   today = datetime.datetime.today()
+
+  import sys
+  #print >> sys.stderr, friends_list.query
+  #friends_list = json.dumps(friends_list)
   friends_list=str(friends_list).replace('["','').replace('"]','').replace(',','').replace('[','').replace(']','').replace("'",'')
 
 
@@ -40,16 +64,32 @@ def get_queryset(request):
   friends_list= friends_list.split()
 
 
-  objects= Ticket.objects.filter(vz_id__in=friends_list).filter(date_validity__gte=today).values('user_details','question', 'item', 'description','date_created','date_validity','ticket_id','vz_id')
-  
+
+
+  #friends_list = json.dumps(friends_list)
+
+  #print >> sys.stderr, friends_list
+
+
+
+  objects= Ticket.objects.filter(vz_id__in=friends_list).filter(date_validity__gte=today)
+
   print >> sys.stderr, objects.query
   print >> sys.stderr, objects
- 
-  from django.http import HttpResponse
-  return HttpResponse(objects,content_type='application/json')
+  #objects= Ticket.objects.filter(vz_id__in=['VZ1445062511', 'VZ1445062656', 'VZ1445613566', 'VZ1445613959'])
+  #print objects.query
+#VZ1445062511, VZ1445062656, VZ1445613566, VZ1445613959
+  # import pdb; 
+  # pdb.set_trace()
 
   
-  #return objects
+
+
+
+
+  #.filter(date_validity__gte=today)
+  
+  return objects
 
 
   # import os
