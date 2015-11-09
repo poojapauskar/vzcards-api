0fdcf92be74340b2c89bdbcf612a4f2b1fe2d0e8
poojapauskar <git.poojapauskar@gmail.com>
2015-11-09 11:27:47 +0530

    changes in user details feeds

--- get_list/views.py
+++ get_list/views.py
@@ -87,7 +87,7 @@ def get_queryset(request):
               {
                'question':list(Ticket_create.objects.filter(ticket_id=obj1.ticket_id).filter(date_validity__gte=today).values_list('question')), 
                'feeds':list(Ticket_create.objects.filter(ticket_id=obj1.ticket_id).filter(date_validity__gte=today).values_list('vz_id','item_photo','question', 'item', 'description','date_created','date_validity','ticket_id')),
-               'user_details':list(Register.objects.filter(vz_id=obj1.vz_id).values_list('pk','token_generated','photo','firstname', 'lastname', 'email', 'phone','vz_id','industry','company','address_line_1','address_line_2','city','pin_code','otp_generated')),  
+               'user_details':list(Register.objects.filter(vz_id=obj1.vz_id).values('pk','token_generated','photo','firstname', 'lastname', 'email', 'phone','vz_id','industry','company','address_line_1','address_line_2','city','pin_code','otp_generated')),  
                }
             )
     
