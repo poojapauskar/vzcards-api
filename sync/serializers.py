46372c0abeac332c5bcb3a42211e415e83a565f2
poojapauskar <git.poojapauskar@gmail.com>
2016-04-28 16:45:38 +0530

    printing phone nos. in sync

--- sync/serializers.py
+++ sync/serializers.py
@@ -17,6 +17,8 @@ class SyncSerializer(serializers.ModelSerializer):
         
         #convert array to string
         import json
+        print >> sys.stderr, validated_data.get('contact_list')
+        
         friends_list=list(User_register.objects.filter(phone__in=validated_data.get('contact_list')).values_list('vz_id', flat=True))
         
         friends_list = json.dumps(friends_list)
