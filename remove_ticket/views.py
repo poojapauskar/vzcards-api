009e56b4fff81558af7606ce3d684d2687d08f7f
poojapauskar <git.poojapauskar@gmail.com>
2016-07-15 12:01:50 +0530

    remove tickets removes data of cloudinary and upload image

--- remove_ticket/views.py
+++ remove_ticket/views.py
@@ -1,14 +1,25 @@
 from ticket_create.models import Ticket_create
+from upload_image.models import Upload_image
 from connect.models import Connect
 from remove_ticket.serializers import Remove_ticketSerializer
 from rest_framework import generics
+
 # from ticket.permissions import IsOwnerOrReadOnly
 # from rest_framework import permissions
 from django.shortcuts import get_object_or_404
 
 from django.http import JsonResponse
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
+
+cloudinary.config( 
+  cloud_name = "harnesymz", 
+  api_key = "597912151168824", 
+  api_secret = "u1Dgxp3BDnIgm-PN_gkJ5stDE30" 
+)
 
 class Remove_ticketDetail(generics.ListAPIView):
  serializer_class = Remove_ticketSerializer
@@ -19,12 +30,18 @@ class Remove_ticketDetail(generics.ListAPIView):
 
   objects1=Ticket_create.objects.filter(ticket_id=ticket_id)
 
-  # item_photo_1=Ticket_create.objects.filter(ticket_id=ticket_id).values_list('item_photo')
-  # import sys
-  # item_photo_2= str(item_photo_1[0]).replace("http://res.cloudinary.com/harnesymz/image/upload/","").replace(".jpg","").replace(",","").replace("u","").replace("'","").replace("(","").replace(")","")
-  # print sys.stderr, item_photo_2
-  # cloudinary.api.delete_resources([item_photo_2])
- 
+  if(Ticket_create.objects.filter(ticket_id=ticket_id).values_list('item_photo').exists()):
+    #delete from cloudinary
+    item_photo_1=Ticket_create.objects.filter(ticket_id=ticket_id).values_list('item_photo')
+    import sys
+    item_photo_2= str(item_photo_1[0]).replace("http://res.cloudinary.com/harnesymz/image/upload/","").replace(".jpg","").replace(",","").replace("u","").replace("'","").replace("(","").replace(")","")
+    print sys.stderr, item_photo_2
+    cloudinary.api.delete_resources([item_photo_2])
+    #delete from upload image api
+    item_photo_3 = (str(item_photo_2).replace("vzcards/","").replace(".jpg",""))+".jpg"
+    print sys.stderr,item_photo_3
+    Upload_image.objects.filter(link=item_photo_3).delete()
+
   objects=Ticket_create.objects.filter(ticket_id=ticket_id).delete()
   objects3=(Connect.objects.filter(ticket_id_1=ticket_id)|Connect.objects.filter(ticket_id_2=ticket_id)).delete()
 
